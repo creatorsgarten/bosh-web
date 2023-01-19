f0b65ca7acc5a595e0e96ea202877fc8251389a4
Thai Pangsakulyanont <dtinth@spacet.me>
2023-01-20 02:03:12 +0700

    Add run command

--- playwright.config.ts
+++ playwright.config.ts
@@ -99,10 +99,11 @@ const config: PlaywrightTestConfig = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: 'rnpm run start',
+    port: 3000,
+    reuseExistingServer: true,
+  },
 };
 
 export default config;
